688fcd58362650c2b3629e33000920b46aec5eeb
Ibrahim Abdul Rahim <mrwaim@gmail.com>
2016-02-26 18:46:50 +0800

    init

---
+++ src/Klsandbox/AnnouncementManagement/AnnouncementManagementServiceProvider.php
@@ -0,0 +1,34 @@
+<?php namespace Klsandbox\AnnouncementManagement;
+
+use Illuminate\Support\ServiceProvider;
+
+class AnnouncementManagementServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return [];
+	}
+
+}
